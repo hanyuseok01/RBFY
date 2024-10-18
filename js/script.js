ab410e9a5b03eb371b14e9dab5b7d03cab32a8b6
한유석 <hk544040@gmail.com>
2024-10-18 18:04:54 +0900

    first rbfy

---
+++ js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+    $('.btn_close').on('click', function () {
+        $('.skip_event').fadeOut()
+    })
+})
+
+
+// href 속성이 '#'인 모든 'a' 태그에 대해 클릭 이벤트 추가
+$('a[href="#"]').click(function (event) {
+        // 기본 동작인 페이지 이동을 방지
+        event.preventDefault();
+        });
+
+        /* 토글 스크립트 */
+        $('.family_site > a').on('click', function () {
+        $(this).toggleClass('on');
+        $('.family_site > span').toggleClass('on');
+    }) 
+        /* menu gnb */
+
+        $('.btn_mobile').on('click', function() {
+            $('.menu').toggleClass('active')
+        })
+
+
+
+/* eventclose */
+$('.product_list > li > a').on('click', function (e) {
+    e.preventDefault();
+})
+/* eventclose */
+
+/* tap */
+$('.product_list li').click(function () {
+    var index = $(this).index();
+    console.log(index)
+
+    $('.product_list li').removeClass('active');
+    $('.product_wrap > div').removeClass('active');
+
+    $(this).addClass('active');
+    $('.product_wrap > div').eq(index).addClass('active');
+});
+/* tap */
+
+var swiper = new Swiper(".rbfy_visual", {
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+    /* navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }, */
+});
+
+var swiper = new Swiper(".main_visual", {
+    watchSlidesProgress: true,
+    slidesPerView: 5,
+    breakpoints: {
+    1440: {
+        slidesPerView: 4, // 태블릿
+    },
+    768: {
+        slidesPerView: 4, // 태블릿
+    },
+    430: {
+        slidesPerView: 2, // 모바일
+    },
+}
+});
